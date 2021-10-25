a00a39a80f6b4863cef1bd0603370e79602b42ab
Jhon Muller <87674368+MrJhonny@users.noreply.github.com>
2021-10-25 15:07:56 -0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,9 @@
+from __future__ import with_statement
+import pyautogui as cle
+import time as bov
+
+bov.sleep(1)
+while True:
+    cle.write('Que pasa Larva !!!!')
+    cle.press('Enter') 
+    bov.sleep(1)
